314162538e79ac3ec49d2e357eac5ecb63707249
kusum <kusum@milvik.se>
2025-12-31 16:15:07 +0530

    publishing to git

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_DhLntSEK.mjs';
-import { manifest } from './manifest_jCwIFUzW.mjs';
+import { manifest } from './manifest_Vb4_I9Yg.mjs';
 
 const serverIslandMap = new Map();;
 
